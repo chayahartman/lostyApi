b8d41d1755fc2838d8620c73f8edc53a61d4a23c
hadassaBenShalom <sh2130362@gmail.com>
2022-01-17 18:06:41 +0200

    hadasa

---
+++ projectLostyApi/projectLostyApi/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BL;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace projectLostyApi.Controllers
+{
+    public class UserController : ApiController
+    {
+
+        public HttpResponseMessage GET()
+        {
+            UserBL bl = new UserBL();
+            int count = bl.countUsers();
+            return Request.CreateResponse(HttpStatusCode.OK, count);
+
+        }
+
+    }
+}
